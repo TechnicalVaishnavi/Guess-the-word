72bece2f4411656f57f490cae094cb2d451f84e8
Vaishnavi <vaishnavibcaiii@gmail.com>
2021-09-07 18:05:04 +0530

    code

---
+++ app/src/main/java/com/example/guesstheword/screen/score/ScoreFragment.kt
@@ -0,0 +1,52 @@
+package com.example.guesstheword.screen.score
+
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.example.guesstheword.R
+import com.example.guesstheword.databinding.ScoreFragmentBinding
+import java.util.Observer
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+
+/**
+ * Fragment where the final score is shown, after the game is over
+ */
+class ScoreFragment : Fragment() {
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: ScoreFragmentBinding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.score_fragment,
+            container,
+            false)
+//            viewModelFactory= ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
+//            viewModel=viewModelFactory.create(ScoreViewModel::class.java)
+
+           binding.scoreViewModel=viewModel
+           binding.lifecycleOwner=viewLifecycleOwner
+
+//            viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
+//                     if(playAgain){
+//                       findNavController().navigate(ScoreFragmentDirections.actionRestart())
+//                         viewModel.onPlayAgainComplete()
+//                     }
+//            })
+        return binding.root
+    }
+}
+
+
+
